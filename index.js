9aa9d74b2f67097a1d156b41805d5b87e2d72afd
rahul-1729 <rk9763981@gmail.com>
2024-07-16 11:12:14 +0530

    added sockets

--- index.js
+++ index.js
@@ -9,12 +9,11 @@ const io = socketio(server)
 io.on('connection',(socket)=>{
     // console.log(socket.id)
     
-    socket.on('from_client',()=>{
-       console.log("Event coming from client")
+    socket.on('msg_send',(data)=>{
+       io.emit('msg_rcvd',data);
+  
     })
-    setInterval(()=>{
-       socket.emit('from_server')
-    },2000)
+   
 });
 
 app.use('/',express.static(__dirname+'/public'));
