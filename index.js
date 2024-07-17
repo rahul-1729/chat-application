bebb409c21688c53f1af7c4a90b3914c65db331f
rahul-1729 <rk9763981@gmail.com>
2024-07-17 11:37:22 +0530

    implemented room

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
 const express = require('express')
 const http = require('http')
 const socketio = require('socket.io')
+ 
+const connect = require('./config/database-config');
 
 const app = express()
 const server = http.createServer(app);
 const io = socketio(server)
 
 io.on('connection',(socket)=>{
-    // console.log(socket.id)
-    
-    socket.on('msg_send',(data)=>{
-       io.emit('msg_rcvd',data);
-  
+    socket.on('join_room',(data)=>{
+        socket.join(data.roomid);
     })
+
+    socket.on('msg_send',(data)=>{
+       
+      io.to(data.roomid).emit('msg_rcvd',data);
+      
+
+    })
+     
    
 });
 
+app.set('view engine','ejs');
+
 app.use('/',express.static(__dirname+'/public'));
 
-server.listen(3010,()=>{
-    console.log("Server started")
+app.get('/chat/:roomid',(req,res)=>{
+     res.render('index',{
+        name:"Rahul",
+        id:req.params.roomid
+     })
+})
+
+
+server.listen(3010,async()=>{
+    console.log("Server started");
+    await connect();
+    console.log("mongo db connected")
 })
